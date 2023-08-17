e537052bc3aa1d6bcaa029a1edb64e98ee14fe6d
Bassim <bassim5n@gmail.com>
2023-08-17 14:52:38 -0400

    create `desktopShortcuts` script

---
+++ desktopShortcuts.bat
@@ -0,0 +1,8 @@
+@echo off
+echo [InternetShortcut] >"%userprofile%\desktop\Office365.url"
+echo URL=https://www.office.com/ >>"%userprofile%\desktop\Office365.url"
+
+
+echo [InternetShortcut] >"%userprofile%\desktop\Outlook Online.url"
+echo URL=https://outlook.office.com/ >>"%userprofile%\desktop\Outlook Online.url"
+
