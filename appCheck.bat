04990042a29b462f3cfbfad9e980bf4d050a0992
Bassim <bassim5n@gmail.com>
2023-10-06 17:36:49 -0400

    updated all scripts to goto with direct checks

--- appCheck.bat
+++ appCheck.bat
@@ -1,33 +1,43 @@
 @echo off
 
+
 :: AnyDesk
-if not exist "%ProgramFiles(x86)%\AnyDesk\AnyDesk.exe" (
-    echo AnyDesk is not installed.
-) else (
-    echo AnyDesk is already installed.
-)
+if exist "%ProgramFiles(x86)%\AnyDesk\AnyDesk.exe" goto FoundAnyDesk
+
+echo AnyDesk is not installed.
+goto EndAnyDesk
+
+:FoundAnyDesk
+echo AnyDesk is already installed.
+
+:EndAnyDesk
 
 
 :: Zoom
-if not exist "%APPDATA%\Zoom" (
-    if not exist "%ProgramFiles%\Zoom" (
-        if not exist "%ProgramFiles(x86)%\Zoom" (
-            echo Zoom is not installed.
-        )
-    )
-) else (
-    echo Zoom is already installed.
-)
+if exist "%APPDATA%\Zoom" goto FoundZoom
+if exist "%ProgramFiles%\Zoom" goto FoundZoom
+if exist "%ProgramFiles(x86)%\Zoom" goto FoundZoom
+
+echo Zoom is not installed.
+goto EndZoom
+
+:FoundZoom
+echo Zoom is already installed.
+
+:EndZoom
 
 
 :: Microsoft Teams
-if not exist "%LOCALAPPDATA%\Microsoft\Teams\current\Teams.exe" (
-    if not exist "%APPDATA%\Microsoft\Teams\current\Teams.exe" (
-        echo Microsoft Teams is not installed.
-    )
-) else (
-    echo Microsoft Teams is already installed.
-)
+if exist "%LOCALAPPDATA%\Microsoft\Teams\current\Teams.exe" goto FoundTeams
+if exist "%APPDATA%\Microsoft\Teams\current\Teams.exe" goto FoundTeams
+
+echo Microsoft Teams is not installed.
+goto EndTeams
+
+:FoundTeams
+echo Microsoft Teams is already installed.
+
+:EndTeams
 
 
 :: Google Chrome
@@ -45,49 +55,62 @@ echo Google Chrome is already installed.
 
 
 :: AnyConnect
-if not exist "%ProgramFiles(x86)%\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe" (
-    echo AnyConnect is not installed.
-) else (
-    echo AnyConnect is already installed.
-)
+if exist "%ProgramFiles(x86)%\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe" goto FoundAnyConnect
 
+echo AnyConnect is not installed.
+goto EndAnyConnect
+
+:FoundAnyConnect
+echo AnyConnect is already installed.
+
+:EndAnyConnect
 
 :: Trend Micro
-if not exist "%ProgramFiles(x86)%\Trend Micro" (
-    echo Trend Micro WFBS-SVC is not installed.
-) else (
-    echo Trend Micro is already installed.
-)
+if exist "%ProgramFiles(x86)%\Trend Micro" goto FoundTrendMicro
 
+echo Trend Micro WFBS-SVC is not installed.
+goto EndTrendMicro
+
+:FoundTrendMicro
+echo Trend Micro is already installed.
+
+:EndTrendMicro
 
 :: Webex
-if not exist "%LOCALAPPDATA%\CiscoSparkLauncher\CiscoCollabHost" (
-    if not exist "%APPDATA%\CiscoSparkLauncher\CiscoCollabHost" (
-        echo Webex is not installed.
-    )
-) else (
-    echo Webex is already installed.
-)
+if exist "%LOCALAPPDATA%\CiscoSparkLauncher\CiscoCollabHost" goto FoundWebex
+if exist "%APPDATA%\CiscoSparkLauncher\CiscoCollabHost" goto FoundWebex
 
+echo Webex is not installed.
+goto EndWebex
 
-:: Check for Carbon Black installation
-if not exist "%ProgramFiles%\Confer" (
-    if not exist "%ProgramFiles(x86)%\Confer" (
-        echo Carbon Black is not installed.
-    )
-) else (
-    echo Carbon Black is already installed.
-)
+:FoundWebex
+echo Webex is already installed.
+
+:EndWebex
+
+:: Carbon Black
+if exist "%ProgramFiles%\Confer" goto FoundCarbonBlack
+if exist "%ProgramFiles(x86)%\Confer" goto FoundCarbonBlack
+
+echo Carbon Black is not installed.
+goto EndCarbonBlack
+
+:FoundCarbonBlack
+echo Carbon Black is already installed.
+
+:EndCarbonBlack
 
 :: Content Shield
-if not exist "%ProgramFiles%\Barracuda\Content Shield" (
-    if not exist "%ProgramFiles(x86)%\Barracuda\Content Shield" (
-        echo Content Shield is not installed.
-    )
-) else (
-    echo Content Shield is already installed.
-)
+if exist "%ProgramFiles%\Barracuda\Content Shield" goto FoundContentShield
+if exist "%ProgramFiles(x86)%\Barracuda\Content Shield" goto FoundContentShield
 
+echo Content Shield is not installed.
+goto EndContentShield
+
+:FoundContentShield
+echo Content Shield is already installed.
+
+:EndContentShield
 
 echo Installation checks complete.
 pause
