649cc296e3b5807cfed72b40b4ebe58c6f8afd41
Bassim <bassim5n@gmail.com>
2023-10-09 14:15:39 -0400

    re-ordered app checks

--- appCheck.bat
+++ appCheck.bat
@@ -1,5 +1,29 @@
 @echo off
 
+:: Adobe Acrobat 
+if exist "%ProgramFiles%\Adobe\Acrobat 11.0" goto FoundAcro
+if exist "%ProgramFiles(x86)%\Adobe\Acrobat 11.0" goto FoundAcro
+
+echo Adobe Acrobat is not installed.
+goto EndAcro
+
+:FoundAcro
+echo Adobe Acrobat is already installed.
+
+:EndAcro
+
+
+:: AnyConnect
+if exist "%ProgramFiles(x86)%\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe" goto FoundAnyConnect
+
+echo AnyConnect is not installed.
+goto EndAnyConnect
+
+:FoundAnyConnect
+echo AnyConnect is already installed.
+
+:EndAnyConnect
+
 
 :: AnyDesk
 if exist "%ProgramFiles(x86)%\AnyDesk\AnyDesk.exe" goto FoundAnyDesk
@@ -13,31 +37,43 @@ echo AnyDesk is already installed.
 :EndAnyDesk
 
 
-:: Zoom
-if exist "%APPDATA%\Zoom" goto FoundZoom
-if exist "%ProgramFiles%\Zoom" goto FoundZoom
-if exist "%ProgramFiles(x86)%\Zoom" goto FoundZoom
+::Avaya
+if exist "%ProgramFiles%\Avaya Cloud" goto FoundAvaya
+if exist "%ProgramFiles(x86)%\Avaya Cloud" goto FoundAvaya
 
-echo Zoom is not installed.
-goto EndZoom
+echo Avaya Cloud is not installed.
+goto EndAvaya
 
-:FoundZoom
-echo Zoom is already installed.
+:FoundAvaya
+echo Avaya Cloud is already installed.
 
-:EndZoom
+:EndAvaya
 
 
-:: Microsoft Teams
-if exist "%LOCALAPPDATA%\Microsoft\Teams\current\Teams.exe" goto FoundTeams
-if exist "%APPDATA%\Microsoft\Teams\current\Teams.exe" goto FoundTeams
+:: Carbon Black
+if exist "%ProgramFiles%\Confer" goto FoundCarbonBlack
+if exist "%ProgramFiles(x86)%\Confer" goto FoundCarbonBlack
 
-echo Microsoft Teams is not installed.
-goto EndTeams
+echo Carbon Black is not installed.
+goto EndCarbonBlack
 
-:FoundTeams
-echo Microsoft Teams is already installed.
+:FoundCarbonBlack
+echo Carbon Black is already installed.
 
-:EndTeams
+:EndCarbonBlack
+
+
+:: Content Shield
+if exist "%ProgramFiles%\Barracuda\Content Shield" goto FoundContentShield
+if exist "%ProgramFiles(x86)%\Barracuda\Content Shield" goto FoundContentShield
+
+echo Content Shield is not installed.
+goto EndContentShield
+
+:FoundContentShield
+echo Content Shield is already installed.
+
+:EndContentShield
 
 
 :: Google Chrome
@@ -54,16 +90,31 @@ echo Google Chrome is already installed.
 :EndChrome
 
 
-:: AnyConnect
-if exist "%ProgramFiles(x86)%\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe" goto FoundAnyConnect
+:: Microsoft Teams
+if exist "%LOCALAPPDATA%\Microsoft\Teams\current\Teams.exe" goto FoundTeams
+if exist "%APPDATA%\Microsoft\Teams\current\Teams.exe" goto FoundTeams
 
-echo AnyConnect is not installed.
-goto EndAnyConnect
+echo Microsoft Teams is not installed.
+goto EndTeams
 
-:FoundAnyConnect
-echo AnyConnect is already installed.
+:FoundTeams
+echo Microsoft Teams is already installed.
 
-:EndAnyConnect
+:EndTeams
+
+
+:: Microsoft Office 365
+if exist "%ProgramFiles%\Microsoft Office" goto FoundOffice
+if exist "%ProgramFiles(x86)%\Microsoft Office" goto FoundOffice
+
+echo Microsoft Office is not installed.
+goto EndOffice
+
+:FoundOffice
+echo Microsoft Office is already installed.
+
+:EndOffice
+
 
 :: Trend Micro
 if exist "%ProgramFiles(x86)%\Trend Micro" goto FoundTrendMicro
@@ -75,6 +126,7 @@ goto EndTrendMicro
 echo Trend Micro is already installed.
 
 :EndTrendMicro
+
 
 :: Webex
 if exist "%LOCALAPPDATA%\CiscoSparkLauncher\CiscoCollabHost" goto FoundWebex
@@ -88,66 +140,19 @@ echo Webex is already installed.
 
 :EndWebex
 
-:: Carbon Black
-if exist "%ProgramFiles%\Confer" goto FoundCarbonBlack
-if exist "%ProgramFiles(x86)%\Confer" goto FoundCarbonBlack
 
-echo Carbon Black is not installed.
-goto EndCarbonBlack
+:: Zoom
+if exist "%APPDATA%\Zoom" goto FoundZoom
+if exist "%ProgramFiles%\Zoom" goto FoundZoom
+if exist "%ProgramFiles(x86)%\Zoom" goto FoundZoom
 
-:FoundCarbonBlack
-echo Carbon Black is already installed.
+echo Zoom is not installed.
+goto EndZoom
 
-:EndCarbonBlack
+:FoundZoom
+echo Zoom is already installed.
 
-:: Content Shield
-if exist "%ProgramFiles%\Barracuda\Content Shield" goto FoundContentShield
-if exist "%ProgramFiles(x86)%\Barracuda\Content Shield" goto FoundContentShield
-
-echo Content Shield is not installed.
-goto EndContentShield
-
-:FoundContentShield
-echo Content Shield is already installed.
-
-:EndContentShield
-
-:: Adobe Acrobat 
-if exist "%ProgramFiles%\Adobe\Acrobat 11.0" goto FoundAcro
-if exist "%ProgramFiles(x86)%\Adobe\Acrobat 11.0" goto FoundAcro
-
-echo Adobe Acrobat is not installed.
-goto EndAcro
-
-:FoundAcro
-echo Adobe Acrobat is installed.
-
-:EndAcro
-
-::Avaya
-if exist "%ProgramFiles%\Avaya Cloud" goto FoundAvaya
-if exist "%ProgramFiles(x86)%\Avaya Cloud" goto FoundAvaya
-
-echo Avaya Cloud is not installed.
-goto EndAvaya
-
-:FoundAvaya
-echo Avaya Cloud is installed.
-
-:EndAvaya
-
-
-:: Microsoft Office 365
-if exist "%ProgramFiles%\Microsoft Office" goto FoundOffice
-if exist "%ProgramFiles(x86)%\Microsoft Office" goto FoundOffice
-
-echo Microsoft Office is not installed.
-goto EndOffice
-
-:FoundOffice
-echo Microsoft Office is installed.
-
-:EndOffice
+:EndZoom
 
 
 echo Installation checks complete.
