26ce4128fb210ede9289ff4a115df75301e8cb03
Bassim Shahidy <bassim101@gmail.com>
2025-05-02 13:11:49 -0400

    delete unnecessary scripts

--- AdminPrep.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Launch the PowerShell script with an unrestricted policy and keep the window open
-powershell.exe -NoProfile -ExecutionPolicy Bypass -NoExit -File "%~dp0\AdminPrep.ps1"
